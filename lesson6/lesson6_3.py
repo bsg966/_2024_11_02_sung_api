f6c7226a8de2501b12ef9d82ed4cfc6337bc4fd0
bsg966 <bsg966@gmail.com>
2024-12-07 06:40:44 +0000

    lesson6_3

---
+++ lesson6/lesson6_3.py
@@ -0,0 +1,10 @@
+from pprint import pprint
+import tools
+
+def main():
+    sitenames:list[str] = tools.get_sitenames(excel_name='aqi.xlsx')
+    print(sitenames)
+  
+
+if __name__ == '__main__':
+    main()
